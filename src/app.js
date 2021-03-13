b4bf6ef110f66df98a3e98ce00fbb4611cdd771e
eweagraff <weagraff.emily@gmail.com>
2021-03-13 10:53:19 -0800

    Updated weather icon

--- src/app.js
+++ src/app.js
@@ -39,12 +39,15 @@ currentDay.innerHTML = `${weekDays} ${month} ${date},  ${year} `;
 currentTime.innerHTML = `${time}`;
 
 function displayTemperature(response) {
+ console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#weather-type").innerHTML =
-    response.data.weather[0].main;
+  document.querySelector("#weather-type").innerHTML = response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
+
 }
 
 function citySearch(event) {
